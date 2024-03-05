770c27b3d3d9848b80909477d39d366b68355180
amyrosein <amyroseintz@gmail.com>
2024-03-06 00:37:02 +0330

    L-H price and genre filter for books

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,17 +6,51 @@ use App\Http\Resources\BookCollection;
 use App\Http\Resources\BookResource;
 use App\Models\Book;
 use Illuminate\Http\Request;
-
-use function MongoDB\BSON\toJSON;
+use Illuminate\Database\Eloquent\Builder;
 
 class BookController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return new BookCollection(Book::with(['genre', 'author.city'])->paginate(5));
+//        if($request->filled('s') ){
+//            $search = $request->get('s');
+//            return new BookCollection(Book::with(['genre', 'author.city'])
+//                ->where('title', 'like', "%{$search}%")
+//                ->paginate(5)
+//                ->appends('s', $search)
+//            );
+//        }
+//        return new BookCollection(Book::with(['genre', 'author.city'])->paginate(5));
+
+        $query = Book::with(['genre', 'author.city']);
+        if($request->filled('s') ) {
+            $search = $request->get('s');
+            $query->where('title', 'like', "%{$search}%");
+        }
+
+        if ($request->filled('lp') || $request->filled('hp')){
+            $lp = $request->get('lp', 0);
+            $hp = $request->get('hp', PHP_INT_MAX);
+            $query->havingBetween('price', [$lp, $hp]);
+
+        }
+
+        if ($request->filled('genre')){
+            $genre = $request->get('genre');
+            $query->whereHas('genre', function (Builder $query) use ($genre) {
+                $query->where('name', $genre);
+            })->get();
+        }
+
+        $books = $query->paginate(5);
+
+        $books->appends($request->except('page'));
+
+        return new BookCollection($books);
+
     }
 
     /**
